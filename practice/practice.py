04aaaa8014297d863c09a40e9dd93d532b442e29
Kanishka-shetty <kanishkashetty12@gmail.com>
2024-12-01 15:08:19 +0530

    Running on custom port

--- practice/practice.py
+++ practice/practice.py
@@ -2,6 +2,12 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/user")
+@app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return "<p>Welcome</p>"
+
+#Running on custom port
+if __name__ == '__main__':
+    print("Running on custom port, localhost:5001")
+    app.run(debug=True, port=5001)
+    # print("Running on custom port, localhost:5001")
